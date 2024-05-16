9af0d3facb737735546f631bd29b935368d40726
finesoldatik_ <aminkopsergenov2009@gmail.com>
2024-05-16 18:35:27 +0300

    bug fix

--- src-tauri/src/directory.rs
+++ src-tauri/src/directory.rs
@@ -51,14 +51,6 @@ pub fn mod_exists(version: &str, mod_name: &str) -> bool {
   exists(&mod_path).into()
 }
 
-// #[tauri::command(rename_all = "snake_case")]
-// pub async fn open_directory(version: String) {
-//   let version_path: String = get_path().unwrap() + "/finelauncher/versions/" + &version;
-//   if exists(&version_path) {
-//     Command::new("explorer").arg(&version_path).spawn().unwrap();
-//   }
-// }
-
 pub fn get_exe(p: &str) -> Option<String> {
   let source_file_glob = p;
 
@@ -77,37 +69,39 @@ pub fn get_exe(p: &str) -> Option<String> {
 #[tauri::command]
 pub fn show_in_folder(version: String, dbus_state: State<DbusState>) -> Result<(), String> {
   let version_path: String = get_path().unwrap() + "/finelauncher/versions/" + &version;
-  let dbus_guard = dbus_state.0.lock().map_err(|e| e.to_string())?;
+  if exists(&version_path) {
+    let dbus_guard = dbus_state.0.lock().map_err(|e| e.to_string())?;
 
-  // see https://gitlab.freedesktop.org/dbus/dbus/-/issues/76
-  if dbus_guard.is_none() || version_path.contains(",") {
-    let mut path_buf = PathBuf::from(&version_path);
-    let new_path = match path_buf.is_dir() {
-      true => version_path,
-      false => {
-        path_buf.pop();
-        path_buf.into_os_string().into_string().unwrap()
-      }
-    };
-    Command::new("xdg-open")
-      .arg(&new_path)
-      .spawn()
-      .map_err(|e| format!("{e:?}"))?;
-  } else {
-    // https://docs.rs/dbus/latest/dbus/
-    let dbus = dbus_guard.as_ref().unwrap();
-    let proxy = dbus.with_proxy(
-      "org.freedesktop.FileManager1",
-      "/org/freedesktop/FileManager1",
-      Duration::from_secs(5),
-    );
-    let (_,): (bool,) = proxy
-      .method_call(
+    // see https://gitlab.freedesktop.org/dbus/dbus/-/issues/76
+    if dbus_guard.is_none() || version_path.contains(",") {
+      let mut path_buf = PathBuf::from(&version_path);
+      let new_path = match path_buf.is_dir() {
+        true => version_path,
+        false => {
+          path_buf.pop();
+          path_buf.into_os_string().into_string().unwrap()
+        }
+      };
+      Command::new("xdg-open")
+        .arg(&new_path)
+        .spawn()
+        .map_err(|e| format!("{e:?}"))?;
+    } else {
+      // https://docs.rs/dbus/latest/dbus/
+      let dbus = dbus_guard.as_ref().unwrap();
+      let proxy = dbus.with_proxy(
         "org.freedesktop.FileManager1",
-        "ShowItems",
-        (vec![version_path], ""),
-      )
-      .map_err(|e| e.to_string())?;
+        "/org/freedesktop/FileManager1",
+        Duration::from_secs(5),
+      );
+      let (_,): (bool,) = proxy
+        .method_call(
+          "org.freedesktop.FileManager1",
+          "ShowItems",
+          (vec![version_path], ""),
+        )
+        .map_err(|e| e.to_string())?;
+    }
   }
   Ok(())
 }
@@ -116,27 +110,29 @@ pub fn show_in_folder(version: String, dbus_state: State<DbusState>) -> Result<(
 #[tauri::command]
 pub fn show_in_folder(version: String) -> Result<(), String> {
   let version_path: String = get_path().unwrap() + "\\finelauncher\\versions\\" + &version;
-  #[cfg(target_os = "windows")]
-  {
-    Command::new("explorer")
-      .args(["/select,", &version_path]) // The comma after select is not a typo
-      .spawn()
-      .map_err(|e| e.to_string())?;
-  }
+  if exists(&version_path) {
+    #[cfg(target_os = "windows")]
+    {
+      Command::new("explorer")
+        .args(["/select,", &version_path]) // The comma after select is not a typo
+        .spawn()
+        .map_err(|e| e.to_string())?;
+    }
 
-  #[cfg(target_os = "macos")]
-  {
-    let path_buf = PathBuf::from(&version_path);
-    if path_buf.is_dir() {
-      Command::new("open")
-        .args([&version_path])
-        .spawn()
-        .map_err(|e| e.to_string())?;
-    } else {
-      Command::new("open")
-        .args(["-R", &version_path])
-        .spawn()
-        .map_err(|e| e.to_string())?;
+    #[cfg(target_os = "macos")]
+    {
+      let path_buf = PathBuf::from(&version_path);
+      if path_buf.is_dir() {
+        Command::new("open")
+          .args([&version_path])
+          .spawn()
+          .map_err(|e| e.to_string())?;
+      } else {
+        Command::new("open")
+          .args(["-R", &version_path])
+          .spawn()
+          .map_err(|e| e.to_string())?;
+      }
     }
   }
   Ok(())
