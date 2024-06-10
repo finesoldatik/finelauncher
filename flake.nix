ddb3ccaaf2755821fd04092d3ad172d056a2a8b7
InfiniteCoder <info@infinitecoder.org>
2024-06-10 14:49:43 +0300

    Update flake.nix to make loading versions and mods work

--- flake.nix
+++ flake.nix
@@ -42,6 +42,7 @@
             ''
               export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
               export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+              export GIO_MODULE_DIR=${pkgs.glib-networking}/lib/gio/modules/
             '';
         };
       });
