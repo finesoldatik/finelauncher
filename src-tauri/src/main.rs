85c4b14f28fd62954ad751f78dffb3dc3afb64ca
InfiniteCoder <info@infinitecoder.org>
2024-06-10 14:23:45 +0300

    Fix Discord Presence

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use discord_rich_presence::{activity, DiscordIpc, DiscordIpcClient};
 use chrono::Utc;
+use discord_rich_presence::{activity, DiscordIpc, DiscordIpcClient};
 
 mod download;
 mod run;
@@ -13,26 +13,37 @@ use crate::run::{run_game, terminate_process};
 use crate::unzip::unzip;
 
 fn main() {
-  let client_id = "1249433232915824751";
-  let mut client = DiscordIpcClient::new(client_id).expect("Discord Rich Presence error");
-  client.connect().expect("Discord Rich Presence not connected");
+    let client_id = "1249433232915824751";
+    let mut client = DiscordIpcClient::new(client_id).expect("Discord Rich Presence error");
+    match client.connect() {
+        Ok(()) => {
+            let timestamp = Utc::now().timestamp();
 
-  let timestamp = Utc::now().timestamp();
+            let payload = activity::Activity::new()
+                .details("В лаунчере")
+                .timestamps(discord_rich_presence::activity::Timestamps::new().start(timestamp))
+                .assets(discord_rich_presence::activity::Assets::new().large_image("logo"));
 
-  let payload = activity::Activity::new()
-    .details("В лаунчере")
-    .timestamps(discord_rich_presence::activity::Timestamps::new().start(timestamp))
-    .assets(discord_rich_presence::activity::Assets::new().large_image("logo"));
+            client
+                .set_activity(payload)
+                .expect("Cannot set activity message!");
+        }
+        Err(err) if format!("{}", err) == "Couldn't connect to the Discord IPC socket" => (),
+        Err(..) => panic!("Discord Rich Presence not connected"),
+    };
 
-  client.set_activity(payload).expect("Cannot set activity message!");
-
-  #[cfg(target_os = "linux")]
-  std::env::set_var(
-    "GDK_BACKEND",
-    std::env::var("XDG_SESSION_TYPE").unwrap_or("x11".to_owned()),
-  );
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![download_file, unzip, run_game, terminate_process])
-    .run(tauri::generate_context!())
-    .expect("error while running finelauncher");
+    #[cfg(target_os = "linux")]
+    std::env::set_var(
+        "GDK_BACKEND",
+        std::env::var("XDG_SESSION_TYPE").unwrap_or("x11".to_owned()),
+    );
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            download_file,
+            unzip,
+            run_game,
+            terminate_process
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running finelauncher");
 }
